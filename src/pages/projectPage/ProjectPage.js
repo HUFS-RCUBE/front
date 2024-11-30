3095b812054131280d4e8420d4f962a000dea0ad
yerimi <fhddl1019@naver.com>
2024-11-30 19:19:29 +0900

    feat : 프로젝트 Header title 수정

--- src/pages/projectPage/ProjectPage.js
+++ src/pages/projectPage/ProjectPage.js
@@ -1,56 +1,88 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "./projectComponent/proButton";
-import ProjectCard from "./projectComponent/proCard"; 
+import ProjectCard from "./projectComponent/proCard";
 
 const projectData = [
-  { id: 1, year: '24년도', name: 'SODA', tags: ['React', 'Spring'], description: '현대인의 스트레스 해소를 위한 AI 다이어리', image: 'https://via.placeholder.com/300' },
-  { id: 2, year: '24년도', name: '프로젝트 명', tags: ['React'], description: '프로젝트 간단 소개 작성하기 이 프로젝트를 어떤 서비스입니다', image: 'https://via.placeholder.com/300' },
-  { id: 3, year: '23년도', name: '프로젝트 B', tags: ['React'], description: '23년도 프로젝트 설명', image: 'https://via.placeholder.com/300' },
-  { id: 4, year: '22년도', name: '프로젝트 C', tags: ['Spring'], description: '22년도 프로젝트 설명', image: 'https://via.placeholder.com/300' },
+  {
+    id: 1,
+    year: "24년도",
+    name: "SODA",
+    tags: ["React", "Spring"],
+    description: "현대인의 스트레스 해소를 위한 AI 다이어리",
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 2,
+    year: "24년도",
+    name: "프로젝트 명",
+    tags: ["React"],
+    description: "프로젝트 간단 소개 작성하기 이 프로젝트를 어떤 서비스입니다",
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 3,
+    year: "23년도",
+    name: "프로젝트 B",
+    tags: ["React"],
+    description: "23년도 프로젝트 설명",
+    image: "https://via.placeholder.com/300",
+  },
+  {
+    id: 4,
+    year: "22년도",
+    name: "프로젝트 C",
+    tags: ["Spring"],
+    description: "22년도 프로젝트 설명",
+    image: "https://via.placeholder.com/300",
+  },
 ];
 
 function ProjectPage() {
-  const [selectedCategory, setSelectedCategory] = useState('전체');
+  const [selectedCategory, setSelectedCategory] = useState("전체");
 
-  const projectCounts = projectData.reduce((acc, project) => {
-    acc[project.year] = (acc[project.year] || 0) + 1;
-    return acc;
-  }, { 전체: projectData.length });
+  const projectCounts = projectData.reduce(
+    (acc, project) => {
+      acc[project.year] = (acc[project.year] || 0) + 1;
+      return acc;
+    },
+    { 전체: projectData.length }
+  );
 
-  const filteredProjects = selectedCategory === '전체' 
-    ? projectData
-    : projectData.filter(project => project.year === selectedCategory);
+  const filteredProjects =
+    selectedCategory === "전체"
+      ? projectData
+      : projectData.filter((project) => project.year === selectedCategory);
 
   return (
     <Wrapper>
-      <ExText>프로젝트</ExText>
-      <ExText>지금까지의 R-CUBE를 만나보세요</ExText>
+      <Title>프로젝트</Title>
+      <Title>지금까지의 R-CUBE를 만나보세요</Title>
       <ButtonCon>
-        <Button 
-          text={`전체(${projectCounts['전체']})`} 
-          onClick={() => setSelectedCategory('전체')} 
-          isActive={selectedCategory === '전체'}
+        <Button
+          text={`전체(${projectCounts["전체"]})`}
+          onClick={() => setSelectedCategory("전체")}
+          isActive={selectedCategory === "전체"}
         />
-        <Button 
-          text={`24년도(${projectCounts['24년도']})`} 
-          onClick={() => setSelectedCategory('24년도')} 
-          isActive={selectedCategory === '24년도'}
+        <Button
+          text={`24년도(${projectCounts["24년도"]})`}
+          onClick={() => setSelectedCategory("24년도")}
+          isActive={selectedCategory === "24년도"}
         />
-        <Button 
-          text={`23년도(${projectCounts['23년도']})`} 
-          onClick={() => setSelectedCategory('23년도')} 
-          isActive={selectedCategory === '23년도'}
+        <Button
+          text={`23년도(${projectCounts["23년도"]})`}
+          onClick={() => setSelectedCategory("23년도")}
+          isActive={selectedCategory === "23년도"}
         />
-        <Button 
-          text={`22년도(${projectCounts['22년도']})`} 
-          onClick={() => setSelectedCategory('22년도')} 
-          isActive={selectedCategory === '22년도'}
+        <Button
+          text={`22년도(${projectCounts["22년도"]})`}
+          onClick={() => setSelectedCategory("22년도")}
+          isActive={selectedCategory === "22년도"}
         />
       </ButtonCon>
       <CardCon>
         {filteredProjects.map((project) => (
-          <ProjectCard 
+          <ProjectCard
             key={project.id}
             year={project.year}
             image={project.image}
@@ -67,15 +99,16 @@ function ProjectPage() {
 export default ProjectPage;
 
 const Wrapper = styled.div`
-  background-color: #F3F3F3;
   width: 100vw;
   height: auto;
 `;
 
-const ExText = styled.div`
-  margin-left: 73px;
-  font-size: 30px;
+const Title = styled.h1`
+  text-align: left;
+  font-size: 1.5rem;
+  margin-bottom: 1.5rem;
   color: ${({ theme }) => theme.colors.mainColor};
+  margin-left: 73px;
 `;
 
 const ButtonCon = styled.div`
